83e06d0587369de71f113fae5b0f986920339d1c
stakantin <stakantin@mail.ru>
2013-09-17 21:46:37 +0300

    creation

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ x_stopforumspam
 ===============
 
 http://stopforumspam.com/ addon for atk4
+
+Usage:
+Add $c = $this->add('x_stopforumspam/Controller_StopForumSpam');
+
+Then use one of the three methods:
+$c->checkIp('IP for checking (string)');
+$c->checkEmail('EMAIL for checking (string)');
+$c->checkUserName('IP for checking (string)');
+
+RETURN:
+false - if user is not in the bun list
+or
+true - is user in the bun list
---
+++ lib/Controller/StopForumSpam.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ *Created by Konstantin Kolodnitsky
+ * Date: 17.09.13
+ * Time: 18:33
+ */
+namespace x_stopforumspam;
+class Controller_StopForumSpam extends \AbstractController{
+    function init(){
+        parent::init();
+
+        // add add-on locations to pathfinder
+        $l = $this->api->locate('addons',__NAMESPACE__,'location');
+        $addon_location = $this->api->locate('addons',__NAMESPACE__);
+        $this->api->pathfinder->addLocation($addon_location,array(
+            'php'=>'lib'
+        ))->setParent($l);
+    }
+
+    function checkIP($ip){
+        $obj = json_decode($this->getResponse("http://stopforumspam.com/api?ip=".$ip."&f=json"));
+        if($obj->ip->frequency != 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function checkEmail($email){
+        $xml = simplexml_load_string($this->getResponse("http://stopforumspam.com/api?email=".$email));
+//        return $xml->frequency->__toString();
+        if($xml->frequency->__toString() != '0'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function checkUserName($userName){
+        $xml = simplexml_load_string($this->getResponse("http://stopforumspam.com/api?username=".$userName));
+//        return $xml->frequency->__toString();
+        if($xml->frequency->__toString() != '0'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function getResponse($request){
+        $curl = curl_init($request);
+        curl_setopt ($curl, CURLOPT_HEADER, 0);
+        curl_setopt ($curl, CURLOPT_POST, 0);
+        curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt ($curl, CURLOPT_SSL_VERIFYHOST, 1);
+        $response = curl_exec ($curl);
+        curl_close ($curl);
+        return $response;
+    }
+}
